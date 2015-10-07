805560e1c34f0a297b4c1fc6a4823f8bff6de559
Adam Stokes <adam.stokes@ubuntu.com>
2015-10-07 17:58:28 -0400

    add contacts
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ seeds/contacts.js
@@ -0,0 +1,10 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('contacts').del(),
+
+    // Inserts seed entries
+      knex('contacts').insert({id: 1, email: 'testcontact@tests.com', firstname: 'howdy', lastname: 'hoe'})
+  );
+};
